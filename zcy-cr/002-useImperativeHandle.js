4f3def80d274b38a3299fc44b21edb97630fc997
方盖 <huanghonghui@cai-inc.com>
2024-12-19 19:55:41 +0800

    fix: useImperativeHandle使用

---
+++ zcy-cr/002-useImperativeHandle.js
@@ -0,0 +1,36 @@
+// 子组件
+const PDFPreviewSettingPopover = (props, ref) => {
+  const [marginTopValue, setMarginTopValue] = useState(false);
+
+  const handleMarginReset = () => {};
+
+  useImperativeHandle(ref, () => ({
+    PDFPreviewSettingPopoverState() {
+      return {
+        marginTopValue,
+        setMarginTopValue,
+        handleMarginReset,
+      };
+    },
+  }));
+
+  return <Fragment>111</Fragment>;
+};
+
+export default forwardRef(PDFPreviewSettingPopover);
+
+
+// 父组件
+const Father = () => {
+  const PDFPreviewSettingPopoverRef = useRef();
+
+  const { marginTopValue, setMarginTopValue, handleMarginReset } =
+    PDFPreviewSettingPopoverRef.current?.PDFPreviewSettingPopoverState?.() || {};
+
+  return (
+    <PDFPreviewSettingPopover
+      ref={(r) => (PDFPreviewSettingPopoverRef.current = r)}
+      previewDoc={previewDoc}
+    />
+  );
+};
