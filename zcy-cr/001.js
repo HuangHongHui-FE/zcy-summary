b5ad7655f99bf51bf94c20ebe613516ef4f2465f
方盖 <huanghonghui@cai-inc.com>
2024-12-20 09:52:07 +0800

    feat: 新增commit规范

--- zcy-cr/001.js
+++ zcy-cr/001.js
@@ -29,8 +29,7 @@ checkAbolishHint = async (rows) => {
 
 !(await this.checkAbolishHint(rows));
 
-// 2、
-// wrappedComponentRef使用场景：
+// 2、wrappedComponentRef使用场景：
 // 子组件：被高阶组件包裹，例：子组件表单被Form.create()包裹。
 // 父组件：当子组件被高阶组件包裹后，父组件想拿到子组件的引用，直接用ref={}属性设置，将获取不到，需用wrappedComponentRef={}属性设置。
 // 写法：
@@ -50,3 +49,17 @@ checkAbolishHint = async (rows) => {
 // 3、.filter(Boolean)
 const a = true;
 const ls = [a && {}, { b: 111 }, !a && {}].filter(Boolean);
+
+// 4、commit规范
+// git commit -m "feat: 完成超值购小程序注册登陆的开发"
+// feat （feature，产品新功能，通常是能够让用户觉察到的变化，小到文案或样式修改）
+// fix（bug fix，修复 bug）
+// docs（documentation，更新文档或注释）
+// style（code formatting, missing semi colons, … 代码格式调整，对逻辑无影响：比如为按照 eslint 或团队风格修改代码格 式。注意不是 UI 变更）
+// refactor（重构：代码优化但不影响现有功能或添加功能。比如文件、变量重命名、代码抽象为函数，消除魔法数字等）
+// test（when adding missing tests 单测相关变更）
+// chore（杂项：其他无法归类的变更，比如代码合并）
+// perf （性能提升变更）
+// ci（持续集成脚本相关变更）
+// build（代码构建相关变更：比如修复部署时的构建问题、构建脚本 webpack 或 gulp 相关变更）
+// temp（临时代码：不计入 CHANGELOG，比如必须部署到某种环境才能测试的变更。如测试真机上 transparent title 启动参数是否设置成功）
